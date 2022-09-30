4ab16d1eaebb565e5c4c2caa387c14a32ffdfa59
stephaniepuertas <puertasstephanie@gmail.com>
2022-09-29 20:54:58 -0700

    finished assignment

--- server.py
+++ server.py
@@ -1,17 +1,30 @@
 # Import Flask to allow us to create our app
-from flask import Flask, render_template
+
+from turtle import bgcolor
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
-# need route and method
+@app.route('/')
+def index():
+    return redirect('/play')
+
+
+@app.route('/play')
+def play_default_box():
+    return render_template('index.html', num=3 , bgcolor="blue")
+
+@app.route('/play/<int:num>')
+def play_default_box_num(num):
+    return render_template('index.html', num=num , bgcolor="blue")
+
+
 
 @app.route('/play/<int:num>/<bgcolor>')
 def play(num, bgcolor):
-    return render_template('index.html',num=num, bgcolor=bgcolor)  
+    return render_template('index.html',num=num, bgcolor=bgcolor)
 
-# @app.route('/')
-# def index():
-    return render_template("index.html", phrase="hello", times=5)
+
 
 
 if __name__ == '__main__':
